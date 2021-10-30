df0d2334dbb06bce5f1ed78a830a546f9c89d188
ed <edchapman88@gmail.com>
2021-10-30 14:57:35 +0100

    Add todolists app with a simple initial view and template served from the root url conf.

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,7 +3,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from todolists.views import lists
+
 urlpatterns = [
+    path('', lists, name='todolists'),
     path('admin/', admin.site.urls),
 ]
 
