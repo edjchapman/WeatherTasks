1e4ca867e65d383b91a5abbe2d1b029fd61b8d2a
ed <edchapman88@gmail.com>
2021-11-01 20:36:16 +0000

    Remove the list model to just focus on a single Todo list for the MVP.

--- backend/todolists/models.py
+++ backend/todolists/models.py
@@ -1,33 +1,13 @@
 from django.db import models
-from django.utils.text import slugify
-
-
-class TodoList(models.Model):
-    """
-    To-do list model.
-    """
-    title = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(blank=True)
-
-    def __str__(self):
-        return self.title
-
-    def save(self, *args, **kwargs):
-        self.set_slug()
-        super().save(*args, **kwargs)
-
-    def set_slug(self):
-        self.slug = slugify(self.title)[:50]
 
 
 class Task(models.Model):
     """
     To-do list task model.
     """
-    todo_list = models.ForeignKey("todolists.TodoList", related_name="tasks", on_delete=models.CASCADE)
     description = models.CharField(max_length=100)
     nearest_city = models.CharField(max_length=50)
     complete = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"List=({self.todo_list.title}) Task=({self.description})"
+        return self.description
