a9654e1c3ba371a927caa803e870064a78cddecf
ed <edchapman88@gmail.com>
2021-11-04 09:39:24 +0000

    Update tests to reflect model changes.

--- backend/tasks/tests/test_models.py
+++ backend/tasks/tests/test_models.py
@@ -1,27 +1,9 @@
+import random
+
 from django.test import TestCase
-from django.utils.text import slugify
 
-from tasks.models import TodoList, Task
-
-
-class TodoListModelTestCase(TestCase):
-    """
-    TodoList model tests.
-    """
-
-    def test_a_todo_list_can_be_created_with_a_title(self):
-        todo_list = TodoList.objects.create(title="Holiday List")
-        self.assertEqual(
-            "Holiday List",
-            todo_list.title
-        )
-
-    def test_the_slug_field_is_populated_on_save(self):
-        todo_list = TodoList.objects.create(title="Holiday List")
-        self.assertEqual(
-            slugify(todo_list.title)[:50],
-            todo_list.slug
-        )
+from tasks.form_data.cities import cities
+from tasks.models import Task
 
 
 class TaskModelTestCase(TestCase):
@@ -29,11 +11,10 @@ class TaskModelTestCase(TestCase):
     Task model tests.
     """
 
-    def test_a_task_can_be_added_to_a_list(self):
-        todo_list = TodoList.objects.create(title="Holiday List")
-        Task.objects.create(todo_list=todo_list, description="Eat Ice Cream", nearest_city="Brighton")
-        first_task = todo_list.tasks.first()
-        self.assertEqual(
-            first_task.description,
-            "Eat Ice Cream"
-        )
+    def test_weather_and_temperature_gets_set_on_save(self):
+        t = Task()
+        t.description = "Eat Ice Cream"
+        t.nearest_city = random.choice(cities)
+        t.save()
+        self.assertGreater(t.temperature, 0)
+        self.assertIn(t.weather_colour, ["orange", "blue", "red"])
