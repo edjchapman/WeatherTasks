3695bc0d30580d118e9cb4936a0479eb2519ffa2
ed <edchapman88@gmail.com>
2021-11-04 11:30:44 +0000

    Add tests for task display methods.

--- backend/tasks/tests/test_models.py
+++ backend/tasks/tests/test_models.py
@@ -18,3 +18,23 @@ class TaskModelTestCase(TestCase):
         t.save()
         self.assertGreater(t.temperature, 0)
         self.assertIn(t.weather_colour, ["orange", "blue", "red"])
+
+    def test_weather_colour_display_method_does_not_update_after_the_task_is_complete(self):
+        t = Task()
+        t.description = "Eat Ice Cream"
+        t.nearest_city = random.choice(cities)
+        t.complete = True
+        t.save()
+        Task.objects.update(weather_colour="black")
+        t.refresh_from_db()
+        self.assertEqual(t.get_weather_colour_display(), "black")
+
+    def test_temperature_display_method_does_not_update_after_the_task_is_complete(self):
+        t = Task()
+        t.description = "Eat Ice Cream"
+        t.nearest_city = random.choice(cities)
+        t.complete = True
+        t.save()
+        Task.objects.update(temperature=8000)
+        t.refresh_from_db()
+        self.assertEqual(t.get_temperature_display(), 8000)
